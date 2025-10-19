409a278affc8e134faaef873254a41b289e1fecb
Kris Kurzawa <kurzawa1@yahoo.com>
2025-10-19 16:54:38 -0400

    added algos

---
+++ TwoOutOfThree.js
@@ -0,0 +1,50 @@
+// 2032. Two Out of Three
+// Solved
+// Easy
+// Topics
+// premium lock icon
+// Companies
+// Hint
+// Given three integer arrays nums1, nums2, and nums3, return a distinct array containing all the values that are present in at least two out of the three arrays. You may return the values in any order.
+
+
+// Example 1:
+
+// Input: nums1 = [1,1,3,2], nums2 = [2,3], nums3 = [3]
+// Output: [3,2]
+// Explanation: The values that are present in at least two arrays are:
+// - 3, in all three arrays.
+// - 2, in nums1 and nums2.
+// Example 2:
+
+// Input: nums1 = [3,1], nums2 = [2,3], nums3 = [1,2]
+// Output: [2,3,1]
+// Explanation: The values that are present in at least two arrays are:
+// - 2, in nums2 and nums3.
+// - 3, in nums1 and nums2.
+// - 1, in nums1 and nums3.
+// Example 3:
+
+// Input: nums1 = [1,2,2], nums2 = [4,3,3], nums3 = [5]
+// Output: []
+// Explanation: No value is present in at least two arrays.
+
+
+// Constraints:
+
+// 1 <= nums1.length, nums2.length, nums3.length <= 100
+// 1 <= nums1[i], nums2[j], nums3[k] <= 100
+
+var twoOutOfThree = function (nums1, nums2, nums3) {
+    const result = []
+
+    for (let i = 0; i < nums1.length; i++) {
+        if ((nums2.includes(nums1[i]) || nums3.includes(nums1[i])) && result.includes(nums1[i]) === false) result.push(nums1[i])
+    }
+
+    for (let i = 0; i < nums2.length; i++) {
+        if (nums3.includes(nums2[i]) && result.includes(nums2[i]) === false) result.push(nums2[i])
+    }
+
+    return result
+};
